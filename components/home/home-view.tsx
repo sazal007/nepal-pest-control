ee77548e45c2a38bc1ca25196e5e3bcc500ccb22
sazal <sazals48@gmail.com>
2026-01-02 14:54:32 +0545

    dasf

--- components/home/home-view.tsx
+++ components/home/home-view.tsx
@@ -5,7 +5,7 @@ import { AboutSection } from "@/components/home/AboutSection";
 import { ServicesSection } from "@/components/home/ServicesSection";
 import { TestimonialsSection } from "@/components/home/TestimonialsSection";
 import { BlogSection } from "@/components/home/BlogSection";
-import { ProcessSection } from "@/components/home/ProcessSection";
+// import { ProcessSection } from "@/components/home/ProcessSection";
 import { WhoAreWe } from "@/components/home/WhoAreWe";
 import { usePricing } from "@/hooks/use-pricing";
 import { PricingPlans } from "@/components/pricing/PricingPlans";
@@ -30,7 +30,7 @@ export default function HomeView() {
     <>
       <HomeHero />
       <AboutSection />
-      <ProcessSection />
+      {/* <ProcessSection />   */}
       <WhoAreWe />
       {sortedPricing.length > 0 && <PricingPlans plans={sortedPricing} />}
       <ServicesSection />
